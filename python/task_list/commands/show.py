a48fb934549b2de319e7a4fa67267677af4adfec
Koro1229 <youcry987951@gmail.com>
2024-03-12 14:29:58 +0800

    refactor show.py in commands to use super() to notify that I'm using parent function

--- python/task_list/commands/show.py
+++ python/task_list/commands/show.py
@@ -2,7 +2,7 @@ from task_list.commands.command import Command
 
 class Show(Command):
     def run(self, command_rest) -> None:
-        backend_tasks = self.get()
+        backend_tasks = super().get()
         for project, tasks in backend_tasks.items():
             self.console.print(project)
             for task in tasks:
