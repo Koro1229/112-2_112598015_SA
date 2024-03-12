ba861da79bf352f00f8ac40722bb471f02f186c7
Koro1229 <youcry987951@gmail.com>
2024-03-12 20:35:35 +0800

    add condition to prevent command error

--- python/task_list/commands/add.py
+++ python/task_list/commands/add.py
@@ -5,13 +5,22 @@ class Add(Command):
         sub_command_rest = command_rest.split(" ", 1)
         sub_command = sub_command_rest[0]
         if sub_command == "project":
+            if len(sub_command_rest) == 1:
+                self.console.print("sorry cannot find the rest command")
+                return
             result = self.backend.add_project(sub_command_rest[1])
             if result != "":
                 self.console.print(result)
                 self.console.print()
 
         elif sub_command == "task":
+            if len(sub_command_rest) == 1:
+                self.console.print("sorry cannot find the rest command")
+                return
             project_task = sub_command_rest[1].split(" ", 1)
+            if len(project_task) == 1:
+                self.console.print("sorry cannot find the rest command")
+                return
             result = self.backend.add_task(project_task[0], project_task[1])
             if result != "":
                 self.console.print(result)
