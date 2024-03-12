f35aebbd67e622c37d78e81507fb5450ba641357
Koro1229 <youcry987951@gmail.com>
2024-03-12 14:47:23 +0800

    remove unused part in commands

--- python/task_list/commands/command.py
+++ python/task_list/commands/command.py
@@ -15,11 +15,3 @@ class Command:
 ## get
     def get(self) -> Dict[str, List[Task]]:
         return self.backend.get()
-
-## deadline
-    # def deadline(self, command_line: str) -> None:
-    #     task_deadline = command_line.split(" ", 1)
-    #     result = self.backend.set_deadline(task_deadline[0], task_deadline[1])
-    #     if result != "":
-    #         self.console.print(result)
-    #         self.console.print()
--- python/task_list/commands/show.py
+++ python/task_list/commands/show.py
@@ -6,6 +6,5 @@ class Show(Command):
         for project, tasks in backend_tasks.items():
             self.console.print(project)
             for task in tasks:
-                # self.console.print(f"  [{'x' if task.is_done() else ' '}] {task.id}: {task.description}")
                 self.console.print(task.generate_task_string())
             self.console.print()
