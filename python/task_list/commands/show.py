88ab3a865dd4c1e319a0a4e334e0ee2a6dd559d5
Koro1229 <youcry987951@gmail.com>
2024-03-12 21:00:50 +0800

    make commands will only interact with backend only

--- python/task_list/commands/show.py
+++ python/task_list/commands/show.py
@@ -2,9 +2,6 @@ from task_list.commands.command import Command
 
 class Show(Command):
     def run(self, command_rest) -> None:
-        backend_tasks = self.backend.get()
-        for project, tasks in backend_tasks.items():
-            self.console.print(project)
-            for task in tasks:
-                self.console.print(task.generate_task_string())
-            self.console.print()
+        backend_tasks = self.backend.get_list_string()
+        for tasks in backend_tasks:
+            self.console.print(tasks)
